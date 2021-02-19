0949a07a176901490eb7ae99ab08f0b663e1d361
Kurbanaliev <kurbanalieverlan@gmail.com>
2021-02-19 18:51:44 +0600

    Добваил в HTML окошко для ввода

--- best-game/webapp/views.py
+++ best-game/webapp/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from urllib.parse import parse_qs
+import random
 
 # Create your views here.
+l = list(range(1,10))
+secret = random.sample(l, 4)
+def check_view(request):
+    if request.method == 'GET':
+        return render(request, 'home.html')
+    elif request.method == 'POST':
+        request_body = parse_qs(request.body.decode())
+        try:
+            data = list(map(int, request_body['numbers'][0].split(' ')))
+
+
+
+
